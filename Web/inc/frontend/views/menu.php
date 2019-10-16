0a6d301a0f97ed03c93eb5a7b721d31c0fd5122d
Ersan Terzi <38361156+sanger60@users.noreply.github.com>
2019-10-16 13:37:58 +0300

    Menu.php EMI_WEB

--- Web/inc/frontend/views/menu.php
+++ Web/inc/frontend/views/menu.php
@@ -2,150 +2,150 @@
 			<div class="container">
 				<div class="header-data">
 					<div class="logo">
-						<a href="index.html" title=""><img src="images/logo.png" alt=""></a>
-					</div><!--logo end-->
+						<a href="index.html" title=""><img src="../../../assets/images/logo.png" alt=""></a>
+					</div>
 					<div class="search-bar">
 						<form>
-							<input type="text" name="search" placeholder="Search...">
+							<input type="text" name="search" placeholder="Ara...">
 							<button type="submit"><i class="la la-search"></i></button>
 						</form>
-					</div><!--search-bar end-->
+					</div>
 					<nav>
 						<ul>
 							<li>
 								<a href="index.html" title="">
-									<span><img src="images/icon1.png" alt=""></span>
-									Home
+									<span><img src="../../../assets/images/icon1.png" alt=""></span>
+									Anasayfa
 								</a>
 							</li>
 							<li>
 								<a href="companies.html" title="">
-									<span><img src="images/icon2.png" alt=""></span>
-									Companies
+									<span><img src="../../../assets/images/icon2.png" alt=""></span>
+									Şirketler
 								</a>
 								<ul>
-									<li><a href="companies.html" title="">Companies</a></li>
-									<li><a href="company-profile.html" title="">Company Profile</a></li>
+									<li><a href="companies.html" title="">Şirket</a></li>
+									<li><a href="company-profile.html" title="">Şirket Detay</a></li>
 								</ul>
 							</li>
 							<li>
 								<a href="projects.html" title="">
-									<span><img src="images/icon3.png" alt=""></span>
-									Projects
+									<span><img src="../../../assets/images/icon3.png" alt=""></span>
+									Projeler
 								</a>
 							</li>
 							<li>
 								<a href="profiles.html" title="">
-									<span><img src="images/icon4.png" alt=""></span>
-									Profiles
+									<span><img src="../../../assets/images/icon4.png" alt=""></span>
+									Profiller
 								</a>
 								<ul>
-									<li><a href="user-profile.html" title="">User Profile</a></li>
-									<li><a href="my-profile-feed.html" title="">my-profile-feed</a></li>
+									<li><a href="user-profile.html" title="">Kullanıcı Profil</a></li>
+									<li><a href="my-profile-feed.html" title="">Feed</a></li>
 								</ul>
 							</li>
 							<li>
 								<a href="jobs.html" title="">
-									<span><img src="images/icon5.png" alt=""></span>
-									Jobs
+									<span><img src="../../../assets/images/icon5.png" alt=""></span>
+									İşler
 								</a>
 							</li>
 							<li>
 								<a href="#" title="" class="not-box-open">
-									<span><img src="images/icon6.png" alt=""></span>
-									Messages
+									<span><img src="../../../assets/images/icon6.png" alt=""></span>
+									Mesajlar
 								</a>
 								<div class="notification-box msg">
 									<div class="nt-title">
-										<h4>Setting</h4>
-										<a href="#" title="">Clear all</a>
+										<h4>Ayarlar</h4>
+										<a href="#" title="">Tümünü Temizle</a>
 									</div>
 									<div class="nott-list">
 										<div class="notfication-details">
 							  				<div class="noty-user-img">
-							  					<img src="images/resources/ny-img1.png" alt="">
+							  					<img src="../../../assets/images/resources/ny-img1.png" alt="">
 							  				</div>
 							  				<div class="notification-info">
-							  					<h3><a href="messages.html" title="">Jassica William</a> </h3>
-							  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.</p>
-							  					<span>2 min ago</span>
+							  					<h3><a href="#" title="">Ersan Terzi</a> </h3>
+							  					<p>Mesaj</p>
+							  					<span>2 dak.</span>
 							  				</div><!--notification-info -->
 						  				</div>
 						  				<div class="notfication-details">
 							  				<div class="noty-user-img">
-							  					<img src="images/resources/ny-img2.png" alt="">
+							  					<img src="../../../assets/images/resources/ny-img2.png" alt="">
 							  				</div>
 							  				<div class="notification-info">
-							  					<h3><a href="messages.html" title="">Jassica William</a></h3>
-							  					<p>Lorem ipsum dolor sit amet.</p>
-							  					<span>2 min ago</span>
+							  					<h3><a href="#" title="">Jassica William</a></h3>
+							  					<p>Mesaj</p>
+							  					<span>2 dk.</span>
 							  				</div><!--notification-info -->
 						  				</div>
 						  				<div class="notfication-details">
 							  				<div class="noty-user-img">
-							  					<img src="images/resources/ny-img3.png" alt="">
+							  					<img src="../../../assets/images/resources/ny-img3.png" alt="">
 							  				</div>
 							  				<div class="notification-info">
-							  					<h3><a href="messages.html" title="">Jassica William</a></h3>
-							  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempo incididunt ut labore et dolore magna aliqua.</p>
-							  					<span>2 min ago</span>
+							  					<h3><a href="#" title="">Jassica William</a></h3>
+							  					<p>Mesaj</p>
+							  					<span>2 dak.</span>
 							  				</div><!--notification-info -->
 						  				</div>
 						  				<div class="view-all-nots">
-						  					<a href="messages.html" title="">View All Messsages</a>
+						  					<a href="#" title="">Mesajları Göster</a>
 						  				</div>
 									</div><!--nott-list end-->
-								</div><!--notification-box end-->
+								</div>
 							</li>
 							<li>
 								<a href="#" title="" class="not-box-open">
-									<span><img src="images/icon7.png" alt=""></span>
-									Notification
+									<span><img src="../../../assets/images/icon7.png" alt=""></span>
+									Bildirimler
 								</a>
 								<div class="notification-box">
 									<div class="nt-title">
-										<h4>Setting</h4>
-										<a href="#" title="">Clear all</a>
+										<h4>Ayarlar</h4>
+										<a href="#" title="">Tümünü Temizle</a>
 									</div>
 									<div class="nott-list">
 										<div class="notfication-details">
 							  				<div class="noty-user-img">
-							  					<img src="images/resources/ny-img1.png" alt="">
+							  					<img src="../../../assets/images/resources/ny-img1.png" alt="">
 							  				</div>
 							  				<div class="notification-info">
-							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-							  					<span>2 min ago</span>
+							  					<h3><a href="#" title="">Jassica William</a> Projenize yorum yaptı.</h3>
+							  					<span>2 dak.</span>
 							  				</div><!--notification-info -->
 						  				</div>
 						  				<div class="notfication-details">
 							  				<div class="noty-user-img">
-							  					<img src="images/resources/ny-img2.png" alt="">
+							  					<img src="../../../assets/images/resources/ny-img2.png" alt="">
 							  				</div>
 							  				<div class="notification-info">
-							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-							  					<span>2 min ago</span>
+							  					<h3><a href="#" title="">Jassica William</a> Projenize yorum yaptı.</h3>
+							  					<span>2 dak.</span>
 							  				</div><!--notification-info -->
 						  				</div>
 						  				<div class="notfication-details">
 							  				<div class="noty-user-img">
-							  					<img src="images/resources/ny-img3.png" alt="">
+							  					<img src="../../../assets/images/resources/ny-img3.png" alt="">
 							  				</div>
 							  				<div class="notification-info">
-							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-							  					<span>2 min ago</span>
+							  					<h3><a href="#" title="">Jassica William</a> Projenize yorum yaptı.</h3>
+							  					<span>2 dak.</span>
 							  				</div><!--notification-info -->
 						  				</div>
 						  				<div class="notfication-details">
 							  				<div class="noty-user-img">
-							  					<img src="images/resources/ny-img2.png" alt="">
+							  					<img src="../../../assets/images/resources/ny-img2.png" alt="">
 							  				</div>
 							  				<div class="notification-info">
-							  					<h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-							  					<span>2 min ago</span>
+							  					<h3><a href="#" title="">Jassica William</a> Projenize yorum yaptı</h3>
+							  					<span>2 dak.</span>
 							  				</div><!--notification-info -->
 						  				</div>
 						  				<div class="view-all-nots">
-						  					<a href="#" title="">View All Notification</a>
+						  					<a href="#" title="">Tüm Bildirimler</a>
 						  				</div>
 									</div><!--nott-list end-->
 								</div><!--notification-box end-->
@@ -190,14 +190,14 @@
 									<button type="submit">Ok</button>
 								</form>
 							</div><!--search_form end-->
-							<h3>Setting</h3>
+							<h3>Ayarlar</h3>
 							<ul class="us-links">
-								<li><a href="profile-account-setting.html" title="">Account Setting</a></li>
+								<li><a href="profile-account-setting.html" title="">Hesap Ayarları</a></li>
 								<li><a href="#" title="">Privacy</a></li>
 								<li><a href="#" title="">Faqs</a></li>
 								<li><a href="#" title="">Terms & Conditions</a></li>
 							</ul>
-							<h3 class="tc"><a href="sign-in.html" title="">Logout</a></h3>
+							<h3 class="tc"><a href="sign-in.html" title="">Çıkış Yap</a></h3>
 						</div><!--user-account-settingss end-->
 					</div>
 				</div><!--header-data end-->
