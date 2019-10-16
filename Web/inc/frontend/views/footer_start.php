4cc2df955909357710248de024980361de57eb8e
Ersan Terzi <38361156+sanger60@users.noreply.github.com>
2019-10-16 15:00:06 +0300

    Company Setup to index.php

--- Web/inc/frontend/views/footer_start.php
+++ Web/inc/frontend/views/footer_start.php
@@ -1,8 +1,17 @@
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/popper.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/jquery.mCustomScrollbar.js"></script>
-<script type="text/javascript" src="lib/slick/slick.min.js"></script>
-<script type="text/javascript" src="js/scrollbar.js"></script>
-<script type="text/javascript" src="js/script.js"></script>
+<footer>
+			<div class="footy-sec mn no-margin">
+				<div class="container">
+					<p><img src="../../../assets/images/copy-icon2.png" alt="">Copyright 2019</p>
+				</div>
+			</div>
+        </footer>
+        
+</div>
+<script type="text/javascript" src="../../../assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="../../../assets/js/popper.js"></script>
+<script type="text/javascript" src="../../../assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../assets/js/jquery.mCustomScrollbar.js"></script>
+<script type="text/javascript" src="../../../assets/lib/slick/slick.min.js"></script>
+<script type="text/javascript" src="../../../assets/js/scrollbar.js"></script>
+<script type="text/javascript" src="../../../assets/js/script.js"></script>
 
